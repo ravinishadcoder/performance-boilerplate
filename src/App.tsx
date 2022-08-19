c2df624ceada76f556df292bf2b24a86b9bc6cbe
Ritesh Firodiya <ritesh@Riteshs-MacBook-Pro.local>
2022-08-19 13:30:37 +0530

    repo init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import Counter from "./components/Counter";
+import TodoApp from "./components/TodoApp";
+import "./App.css";
 
 function App() {
+  const [answer, setAnswer] = useState<number>();
+  const [value, setValue] = useState(0);
+
+  const fibo = (n: number): number => {
+    if (n <= 1) return n;
+    return fibo(n - 1) + fibo(n - 1);
+  };
+
+  const calc = () => {
+    console.time("t1");
+    let result = fibo(value);
+    console.timeEnd("t1");
+
+    setAnswer(result);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h3>{answer}</h3>
+        <div>
+          <input
+            type="number"
+            value={value}
+            onChange={(e) => setValue(Number(e.target.value))}
+          />
+          <button onClick={calc}>Calc</button>
+        </div>
+      </div>
+      <Counter />
+      <TodoApp />
     </div>
   );
 }
